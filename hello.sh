d8d63af9714008fb94dbeca06438296375c02ef4
firoz2208 <132645837+firoz2208@users.noreply.github.com>
2024-10-13 10:43:48 +0530

    Create hello.sh

---
+++ hello.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+<<note
+echo "TWS"
+multi line comment
+for your hello sholay script
+note
+
+#this is Sholay script
+
+echo "Gabbar: Kitne aadmi the"
+
+echo "Kalia: 2 Sarkar"
+
+echo ""
+
+echo "Gabbar: Kitne baje se class chalu hai"
+
+date
+
+echo "Gabbar: kitne baje se class chal rahi hai"
+
+uptime
